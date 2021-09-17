3c5fbe543ab43feb5738fbb55bfeb5b9a93ae8b4
Erik Jung <erik.jung@5pectre.com>
2021-09-17 11:36:45 +0200

    added Login functionality

---
+++ params/params-min.js
@@ -0,0 +1,1 @@
+const DATABASECONNECTION="mongodb+srv://erikjung:king1645@antern.2mwzc.mongodb.net/users?retryWrites=true&w=majority";module.exports={DATABASECONNECTION:DATABASECONNECTION};
--- routes/web/index-min.js
+++
@@ -1,1 +0,0 @@
-var express=require("express"),router=express.Router();router.use("/",require("./home")),module.exports=router;
